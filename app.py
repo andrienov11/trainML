15c4b95092dfc87a821d630b69697a92bbe33331
Novandri <76807883+andrienov11@users.noreply.github.com>
2026-05-11 20:06:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -241,56 +241,6 @@ li[aria-selected="true"] {
     font-weight: bold !important;
 }
 
-/* =========================================================
-TRAIN BUTTON
-========================================================= */
-
-div[data-testid="stButton"] > button[kind="secondary"] {
-
-    width: 320px !important;
-
-    height: 60px !important;
-
-    margin-left: auto !important;
-    margin-right: auto !important;
-
-    display: block !important;
-
-    border-radius: 18px !important;
-
-    border: none !important;
-
-    background: linear-gradient(
-        135deg,
-        #2563EB,
-        #3B82F6
-    ) !important;
-
-    color: white !important;
-
-    font-size: 18px !important;
-
-    font-weight: 700 !important;
-
-    box-shadow: 0px 6px 20px rgba(37,99,235,0.35) !important;
-
-    transition: all 0.3s ease !important;
-}
-
-/* Hover */
-div[data-testid="stButton"] > button[kind="secondary"]:hover {
-
-    transform: translateY(-3px);
-
-    background: linear-gradient(
-        135deg,
-        #1D4ED8,
-        #2563EB
-    ) !important;
-
-    color: white !important;
-}
-
 </style>
 """, unsafe_allow_html=True)
 
